23cb2c5bcae2d1d813e00fbdba89615ab4b8e488
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-06-28 23:18:40 -0400

    working score method and UI

--- ScrabbleScorer/Models/Scorer.cs
+++ ScrabbleScorer/Models/Scorer.cs
@@ -1,26 +1,26 @@
 using System.Collections.Generic;
 using System;
 
-namespace ScrabbleScorer.Models {
-
-  public static class ScrabbleScorer
+namespace ScrabbleScorer.Models 
+{
+  public class ScrabbleWord
   {
 
     public static Dictionary<string, int> LetterScore = new Dictionary<string, int> 
     {{"A", 1}, {"E", 1}, {"I", 1}, {"O", 1}, {"U", 1}, {"L", 1}, {"N", 1}, {"R", 1}, {"S", 1}, {"T", 1}, {"D", 2}, {"G", 2}, {"B", 3}, {"C", 3}, {"M", 3}, {"P", 3}, {"F", 4}, {"H", 4}, {"V", 4}, {"W", 4}, {"Y", 4}, {"K", 5}, {"J", 8}, {"X", 8}, {"Q", 10}, {"Z", 10}};
 
-    public string Input { get; set ;}
+    public string Input { get; set; }
 
-    public Scrabble(string input)
+    public ScrabbleWord(string input)
     {
       Input = input;
     }
 
-    public int FindWordScore(string input)
+    public int FindWordScore()
     {
       int wordScoreCounter = 0;
-      char[] userWordArray = input.ToUpper().ToCharArray();
-      for (int i = 0; i < userWordArray.length; i++)
+      char[] userWordArray = Input.ToUpper().ToCharArray();
+      for (int i = 0; i < userWordArray.Length; i++)
       {
         foreach(KeyValuePair<string, int> item in LetterScore)
         {
@@ -30,6 +30,7 @@ namespace ScrabbleScorer.Models {
           }
         }
       }
+      return wordScoreCounter;
     }
   }
 }
--- ScrabbleScorer/Program.cs
+++ ScrabbleScorer/Program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using ScrabbleScorer.Models
+using ScrabbleScorer.Models;
 
 namespace ScrabbleScorer
 {
@@ -11,8 +11,13 @@ namespace ScrabbleScorer
     Console.WriteLine("Welcome to the premier Scrabble score checker!");
     Console.WriteLine("Please enter a word to see the Scrabble value!");
     string userInput = Console.ReadLine();
-    
-  }
+    ScrabbleWord newWord = new ScrabbleWord(userInput);
 
+    int wordScore = newWord.FindWordScore();
+    Console.WriteLine("That word is worth " + wordScore + " points!");
+
+    Main();
+  }
+  
 }
 }
