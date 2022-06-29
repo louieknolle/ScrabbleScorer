4c769094f0895016cb127278b4db388555363a8c
Louis Knolle <knollelw@gmail.com>
2022-06-28 21:18:55 -0400

    write static score method

--- ScrabbleScorer/Models/Scorer.cs
+++ ScrabbleScorer/Models/Scorer.cs
@@ -6,20 +6,30 @@ namespace ScrabbleScorer.Models {
   public static class ScrabbleScorer
   {
 
-    private static Dictionary<string, int> LetterScore = new Dictionary<string, int> 
+    public static Dictionary<string, int> LetterScore = new Dictionary<string, int> 
     {{"A", 1}, {"E", 1}, {"I", 1}, {"O", 1}, {"U", 1}, {"L", 1}, {"N", 1}, {"R", 1}, {"S", 1}, {"T", 1}, {"D", 2}, {"G", 2}, {"B", 3}, {"C", 3}, {"M", 3}, {"P", 3}, {"F", 4}, {"H", 4}, {"V", 4}, {"W", 4}, {"Y", 4}, {"K", 5}, {"J", 8}, {"X", 8}, {"Q", 10}, {"Z", 10}};
 
-    char[] userWordArray = userInput.ToCharArray();
-    foreach (var word in userWordArray)
+    public string Input { get; set ;}
+
+    public Scrabble(string input)
     {
-     Console.WriteLine(word);
-    }
-    
-    static void SplitWordValue()
-    {
-      
+      Input = input;
     }
 
-
+    public int FindWordScore(string input)
+    {
+      int wordScoreCounter = 0;
+      char[] userWordArray = input.ToUpper().ToCharArray();
+      for (int i = 0; i < userWordArray.length; i++)
+      {
+        foreach(KeyValuePair<string, int> item in LetterScore)
+        {
+          if(item.Key.Contains(userWordArray[i]))
+          {
+            wordScoreCounter += item.Value;
+          }
+        }
+      }
+    }
   }
 }
